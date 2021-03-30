0378ee18331de87b7734e65490c74eea107e78e0
Krokus-lab <piotrus991@wp.pl>
2021-03-30 18:59:28 +0200

    models
    user and tasks models changes

--- forum/mysite/forum/models.py
+++ forum/mysite/forum/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
+    numer_kontaktowy = models.CharField(max_length=15,blank=True)
+    szkola = models.CharField(max_length=100,blank=True)
+    ranga = models.CharField(max_length=40,blank=True)
     def __str__(self):
         return self.name
 
@@ -27,6 +30,7 @@ class Zadanie_otwarte(models.Model):
     odpowiedz = models.CharField(max_length=30)
     dzial=models.CharField(max_length=30)
     punkty=models.IntegerField()
+    url = models.CharField(max_length=200,blank=True,null=True)
     def __str__(self):
         return self.tresc
 
@@ -41,6 +45,7 @@ class Zadanie_zamkniete(models.Model):
     odpowiedz = models.CharField(max_length=40)
     dzial=models.CharField(max_length=30)
     punkty=models.IntegerField()
+    url = models.CharField(max_length=200,blank=True,null=True)
     def __str__(self):
         return self.tresc
 
