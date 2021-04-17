e46fa1fbc44eacea705a1baf59432361c6b76e8b
kacperPankiewicz <kacper.pankiewicz@wp.pl>
2021-04-17 23:33:18 +0200

    finalInputs

--- forum/mysite/forum/static/forum/js/addQuestion/addQuestion.js
+++ forum/mysite/forum/static/forum/js/addQuestion/addQuestion.js
@@ -4,6 +4,19 @@ createMathquill("A");
 createMathquill("B");
 createMathquill("C");
 createMathquill("D");
+function clearInputs(){
+    $("div[id^='QueDiv']").css("outline","0");
+    $("div[id^='QueDiv']").css("-webkit-box-shadow","none");
+    $("div[id^='QueDiv']").css("box-shadow","none");
+    $("div[id^='QueDiv']").css("border-color","white");
+}
+function focusInput(name){
+    $(name).css("border-color","#66afe9");
+    $(name).css("outline","0");
+    $(name).css("-webkit-box-shadow","0 0 0 0.2rem rgba(102,175,233,.5)");
+    $(name).css("box-shadow","0 0 0 0.2rem rgba(102,175,233,.5)");
+    $(name).css("border-color","#66afe9");
+}
 function createMathquill(id)
 {
 
@@ -15,54 +28,73 @@ handlers: {
         }
     }
 });
-$(document).click(function(e){
-    
-})
-$('#QueDiv'+id ).click( function(e) {
-    $("div[id^='QueDiv']").css("outline","0");
-    $("div[id^='QueDiv']").css("-webkit-box-shadow","0 0 0 0.2rem rgba(102,175,233,.4)");
-    $("div[id^='QueDiv']").css("box-shadow","0 0 0 0.2rem rgba(102,175,233,.4)");
-    $("div[id^='QueDiv']").css("border-color","#66afe9");
-    answerMathField2.focus();
-    $(this).css("border-color","#66afe9");
-    
-    
-   
-    
-});
 
+$('#QueDiv'+id ).click( function(e) {
+    clearInputs();
+    answerMathField2.focus();
+    focusInput(this);
+});
+$( "#sqrt"+id).click(function() {
+    clearInputs();
+    answerMathField2.write('\\sqrt{}');
+    answerMathField2.reflow();
+    answerMathField2.focus();
+    focusInput('#QueDiv'+id);
+});
 
 $( "#mld"+id).click(function() {
-answerMathField2.write('\\cdot');
-answerMathField2.reflow();
+    clearInputs();
+    answerMathField2.write('\\cdot');
+    answerMathField2.reflow();
+    answerMathField2.focus();
+    focusInput('#QueDiv'+id);
 });
 $( "#plus"+id).click(function() {
-answerMathField2.write('+');
-answerMathField2.reflow();
+    clearInputs();
+    answerMathField2.write('+');
+    answerMathField2.reflow();
+    answerMathField2.focus();
+    focusInput('#QueDiv'+id);
 });
 $( "#minus"+id).click(function() {
-answerMathField2.write('-');
-answerMathField2.reflow();
+    clearInputs();
+    answerMathField2.write('-');
+    answerMathField2.reflow();
+    answerMathField2.focus();
+    focusInput('#QueDiv'+id);
 });
 $( "#przecinek"+id).click(function() {
-answerMathField2.write(',');
-answerMathField2.reflow();
+    clearInputs();
+    answerMathField2.write(',');
+    answerMathField2.reflow();
+    answerMathField2.focus();
+    focusInput('#QueDiv'+id);
 });
 $( "#pi"+id).click(function() {
-answerMathField2.write('\\pi');
-answerMathField2.reflow();
+    clearInputs();
+    answerMathField2.write('\\pi');
+    answerMathField2.reflow();
+    answerMathField2.focus();
+    focusInput('#QueDiv'+id);
 });
 $( "#dzielenie"+id ).click(function() {
-answerMathField2.write('\\frac{ }{ }');
-answerMathField2.reflow();
+    clearInputs();
+    answerMathField2.write('\\frac{ }{ }');
+    answerMathField2.reflow();
+    answerMathField2.focus();
+    focusInput('#QueDiv'+id);
 });
 $( "#potega"+id ).click(function() {
-answerMathField2.write('\\^{}');
-answerMathField2.reflow();
+    clearInputs();
+    answerMathField2.write('\\^{}');
+    answerMathField2.reflow();
+    answerMathField2.focus();
 });
 $( "#cbrt"+id).click(function() {
-answerMathField2.write('\\sqrt[]{}');
-answerMathField2.reflow();
+    clearInputs();
+    answerMathField2.write('\\sqrt[]{}');
+    answerMathField2.reflow();
+    answerMathField2.focus();
 });
 
 }
