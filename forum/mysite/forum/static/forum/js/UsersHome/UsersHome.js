d831fcce9a9afb76709f3cfa768e0cef6013f127
kacperPankiewicz <kacper.pankiewicz@wp.pl>
2021-04-25 12:18:15 +0200

    loaderFinished

--- forum/mysite/forum/static/forum/js/UsersHome/UsersHome.js
+++ forum/mysite/forum/static/forum/js/UsersHome/UsersHome.js
@@ -7,3 +7,32 @@ $(window).resize(function(){
         $('#splitter').addClass("w-100");
     }
 });
+
+$('#buttonHistory').click(function(){
+    ajaxCall1();
+})
+function ajaxCall1() {
+    var s = $('#buttonHistory').attr("data-url");
+    $('body').removeClass("loaded");
+    return  $.ajax({
+        url: s,
+        success: function(result){
+            $('body').html(result);
+        }
+    });
+}   
+$('#buttonForum').click(function(){
+    ajaxCall2();
+})
+function ajaxCall2() {
+    var s = $('#buttonForum').attr("data-url");
+    $('body').removeClass("loaded");
+    return  $.ajax({
+        url: s,
+        success: function(result){
+            $('body').html(result);
+        }
+    });
+}   
+
+
